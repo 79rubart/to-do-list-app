3f3178eeddf8e8c299cfecb6caf3c990b2d6a546
Rubens Oliveira <rubensoliveira.prof@gmail.com>
2022-10-25 18:13:10 -0300

    add, scratch and remove task functionalities added

--- src/main.js
+++ src/main.js
@@ -3,18 +3,47 @@ const addBtn = document.querySelector('#submit');
 const divList = document.querySelector('.task-list');
 
 function addLists(){
-    const ul = divList.querySelector('ul');
-    const li = document.createElement('li');
-    li.innerHTML = addInput.value;
-    addInput.value = '';
-    ul.appendChild(li);
+    if (addInput.value === ''){
+        alert('Please add a task!');
+    } else {
+        const ul = divList.querySelector('.list-of-tasks');
+        const li = document.createElement('li');
+        const checkbox = document.createElement('input');
+        const label = document.createElement('label');
+        const close = document.createElement('input');
+
+        checkbox.type = "checkbox";
+        checkbox.id = "checkbox";
+        label.id = "task-name";
+        close.type = "button";
+        close.id = "close";
+
+        label.innerHTML = addInput.value;
+        addInput.value = '';
+        ul.appendChild(li);
+        li.appendChild(checkbox);
+        li.appendChild(label);
+        li.appendChild(close);
+
+        checkbox.addEventListener('change', function(event) {
+            if (event.currentTarget.checked){
+                label.style.textDecoration="line-through";
+            } else {
+                label.style.textDecoration="none";
+            }
+        });
+
+        close.addEventListener('click', function(){
+            li.remove();
+        });
+    }
 }
 
-addBtn.addEventListener('click', () => {
+addBtn.addEventListener('click', function() {
     addLists();
 });
 
-addInput.addEventListener('keyup', (event) => {
+addInput.addEventListener('keyup', function(event) {
     if(event.which === 13) {
         addLists();
     }
