d7a4a9d17d248cb44ed3fb9dedf458a613ff8ec1
Rubens Oliveira <rubensoliveira.prof@gmail.com>
2022-10-24 23:55:34 -0300

    add item to list function added

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
-document.getElementById("submit").onclick  = function() {
+const addInput = document.querySelector('#add-task');
+const addBtn = document.querySelector('#submit');
+const divList = document.querySelector('.task-list');
 
-    var node = document.createElement(li);
-    var text = document.getElementById("add-task").value; 
-    var textnode = document.createTextNode(text);
-    node.appendChild(textnode);
-    document.getElementById("list-of-tasks").appendChild(node);
+function addLists(){
+    const ul = divList.querySelector('ul');
+    const li = document.createElement('li');
+    li.innerHTML = addInput.value;
+    addInput.value = '';
+    ul.appendChild(li);
 }
+
+addBtn.addEventListener('click', () => {
+    addLists();
+});
+
+addInput.addEventListener('keyup', (event) => {
+    if(event.which === 13) {
+        addLists();
+    }
+});
